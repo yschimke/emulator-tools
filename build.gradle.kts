8e727e23652f91759f8a190bdb0ed1845550a590
Yuri Schimke <yuri@schimke.ee>
2020-04-19 11:52:00 +0100

    Fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,16 @@
+import com.google.protobuf.gradle.protobuf
+import com.google.protobuf.gradle.protoc
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.12")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+    }
+}
+
 plugins {
     kotlin("jvm") version "1.3.71"
     `maven-publish`
@@ -5,6 +18,7 @@ plugins {
     id("com.github.ben-manes.versions") version "0.28.0"
     id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
     id("net.nemerosa.versioning") version "2.8.2"
+    id("com.google.protobuf") version "0.8.12"
 }
 
 repositories {
@@ -43,6 +57,30 @@ dependencies {
     implementation("io.grpc:grpc-netty-shaded:1.26.0")
     implementation("io.grpc:grpc-protobuf:1.26.0")
     implementation("io.grpc:grpc-stub:1.26.0")
+}
+
+protobuf {
+    protoc { artifact = "com.google.protobuf:protoc:3.11.4" }
+//    plugins {
+        // Specify protoc to generate using kotlin protobuf plugin
+//        grpc {
+//            artifact = "io.grpc:protoc-gen-grpc-java:1.28.1"
+//        }
+//        // Specify protoc to generate using our grpc kotlin plugin
+//        grpckt {
+//            artifact = "io.grpc:protoc-gen-grpc-kotlin:0.1.1"
+//        }
+//    }
+//    generateProtoTasks {
+//        all().each { task ->
+//            task.plugins {
+//                // Generate Java gRPC classes
+//                grpc { }
+//                // Generate Kotlin gRPC using the custom plugin from library
+//                grpckt { }
+//            }
+//        }
+//    }
 }
 
 val sourcesJar by tasks.registering(Jar::class) {
